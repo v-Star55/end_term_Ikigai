85a4f11738abc1ab8da14254510ecc3200a27989
Vaibhav Chauhan <vaibhavchauhanrajput74321@gmail.com>
2023-12-14 16:27:06 +0530

    done

--- src/Footer.js
+++ src/Footer.js
@@ -6,10 +6,10 @@ const Footer = () => {
     <div class="footer-basic">
         <footer>
             <div class="social">
-                <a href="www.instagram.com"><i class="icon ion-social-instagram"></i>
-                </a><a href="#"><i class="icon ion-social-snapchat"></i>
-                </a><a href="#"><i class="icon ion-social-twitter"></i>
-                </a><a href="#"><i class="icon ion-social-facebook"></i></a>
+                <a href="https://www.instagram.com"><i class="icon ion-social-instagram"></i>
+                </a><a href="https://www.snapchat.com"><i class="icon ion-social-snapchat"></i>
+                </a><a href="https://www.x.com"><i class="icon ion-social-twitter"></i>
+                </a><a href="https://www.facebook.com"><i class="icon ion-social-facebook"></i></a>
                 </div>
             <div class="location">
             <h1 class="tex-center">Location</h1>
